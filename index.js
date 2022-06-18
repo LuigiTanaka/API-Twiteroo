2772b04cfefb2bfb642244f4f6bc3b6f06755f89
luigi tanaka <luigi.tanaka12@gmail.com>
2022-06-17 21:39:46 -0300

    projeto final sem bônus

--- index.js
+++ index.js
@@ -4,7 +4,44 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+const usuarios = [];
+const tweets = [];
+let foto;
+
+app.post("/sign-up", (req, res) => {
+    const { username, avatar } = req.body;
+    foto = avatar;
+    if (username && avatar) {
+        usuarios.push({ username, avatar });
+        res.send("OK");
+    } else {
+        res.send("ERROR");
+    }
+});
+
+app.post("/tweets", (req, res) => {
+    const { username, tweet } = req.body;
+    if (username && tweet) {
+        tweets.push({ username, avatar: foto, tweet });
+        res.send("OK");
+    } else {
+        res.send("ERROR");
+    }
+});
+
+app.get("/tweets", (req, res) => {
+    const tamanho = tweets.length;
+    if (tamanho > 10) {
+        res.send(tweets.slice(tamanho-10, tamanho).reverse());
+    } else {
+        res.send(tweets.reverse());
+    }
+    
+})
+
 
 app.listen(5000, () => {
-    console.log(chalk.green("estou rodando!"));
+    console.log(chalk.bold.green("estou rodando!"));
 });
